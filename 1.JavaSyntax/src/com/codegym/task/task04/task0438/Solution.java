bfccffe6262a15b64c10437de1abe01524e42676
Kikikidman <Fahrenheit1806@mail.ru>
2019-02-05 10:35:17 +0300

    task 04 37 finish

---
+++ 1.JavaSyntax/src/com/codegym/task/task04/task0438/Solution.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task04.task0438;
+
+/* 
+Drawing lines
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 1; i++){
+            System.out.print("8888888888");
+            for (int j = 0; j < 10; j++){
+                System.out.println("8");
+            }
+        }
+    }
+}
+
