7a13d1616254176b49fd336c5ebf0b412a1fc336
Kikikidman <Fahrenheit1806@mail.ru>
2019-02-05 14:56:29 +0300

    lvl 5!

---
+++ 1.JavaSyntax/src/com/codegym/task/task04/task0443/Solution.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task04.task0443;
+
+
+/* 
+A name is a name
+
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int year = scanner.nextInt();
+        int month = scanner.nextInt();
+        int day = scanner.nextInt();
+
+        System.out.println("My name is " + name + ".");
+        System.out.print("I was born on " + month + "/" + day + "/" + year);
+    }
+}
