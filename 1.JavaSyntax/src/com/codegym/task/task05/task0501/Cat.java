862d6c9f9814284811e4c8f3cda7867b1e751879
Kikikidman <Fahrenheit1806@mail.ru>
2019-02-05 17:03:38 +0300

    task 0502 passed

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0501/Cat.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task05.task0501;
+
+/* 
+Creating a cat
+
+*/
+
+import java.util.Scanner;
+
+public class Cat {
+    String name;
+    int age;
+    int weight;
+    int strength;
+
+    public static void main(String[] args) {
+
+    }
+}
