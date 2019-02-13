f37671bee0cc9731009c3483879ede11a7a52a12
Kikikidman <Fahrenheit1806@mail.ru>
2019-02-13 12:53:51 +0300

    task 0617 in progress

---
+++ 1.JavaSyntax/src/com/codegym/task/task06/task0617/Solution.java
@@ -0,0 +1,14 @@
+package com.codegym.task.task06.task0617;
+
+/* 
+Notepad for new ideas
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        printIdea(new Idea());
+    }
+
+    //write your code here
+}
