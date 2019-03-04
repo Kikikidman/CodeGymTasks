2fd3cfb9d13ba4c2684a256e9759be5361b8d9da
Kikikidman <Fahrenheit1806@mail.ru>
2019-03-04 13:13:54 +0300

    task 0918 finish (Exceptions)

---
+++ 1.JavaSyntax/src/com/codegym/task/task09/task0906/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task09.task0906;
+
+/* 
+Logging stack traces
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        log( "In main method");
+    }
+
+    public static void log(String s) {
+        String a = Thread.currentThread().getStackTrace()[2].getClassName();
+        String b = Thread.currentThread().getStackTrace()[2].getMethodName();
+
+        System.out.println(a + ": " + b + ": " + s);
+    }
+}
